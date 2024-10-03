9d586c25c6d16862aa7d51b54b771ee71e5d9e93
devscarycat <qkymzh123@gmail.com>
2024-10-03 12:39:14 +0900

    9.0 Product Page

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,25 @@
-export default function Products() {
+import ListProduct from "@/components/list-Product";
+import PrismaDB from "@/lib/db";
+
+async function getProduct() {
+    const products = await PrismaDB.product.findMany({
+        select: {
+            id: true,
+            title: true,
+            price: true,
+            created_at: true,
+            photo: true,
+        }
+    })
+    return products
+}
+export default async function Products() {
+    const products = await getProduct();
     return (
-        <div>
-            <h1 className="text-white text-4xl">Products!</h1>
+        <div className="p-5 flex flex-col gap-5">
+            {products.map((product) => (
+                <ListProduct key={product.id} {...product} />
+            ))}
         </div>
     );
 }
